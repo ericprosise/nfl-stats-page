e51073ef7e9dbd51a5709c2824254fc37c7bc786
CORP\prosiser <Eric.S.Prosise@espn.com>
2019-08-15 21:04:08 -0400

    Add Kendo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from "./components/Header";
 import Stats from './components/Stats';
 import { default as player } from './statOutput';
+// You can import style files in ./App.js and add global styles in ./App.css
+import '@progress/kendo-theme-default/dist/all.css';
 
 function App() {
   console.log('player', player);
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,30 +1,107 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import {
+    Chart,
+    ChartTitle,
+    ChartSeries,
+    ChartSeriesItem,
+    ChartCategoryAxis,
+    ChartCategoryAxisItem,
+    ChartTooltip
+} from '@progress/kendo-react-charts';
 
-function Stats(props) {
-    console.log(props.stats);
-    return (
-        <div>
-            <table>
-                <tbody>
-                    {props.stats.map((year) => {
-                        return (
-                            <tr key={year.year}>
-                                <td>{year.year}</td>
-                                <td>{year.age}</td>
-                                <td>{year.g}</td>
-                                <td>{year.gs}</td>
-                                <td>{year.qb_rec}</td>
-                                <td>{year.pass_cmp}</td>
-                                <td>{year.pass_yds}</td>
-                                <td>{year.pass_tds}</td>
-                                <td>{year.pass_int}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+
+function transformData(stats) {
+    return stats.map((stat) => {
+        return {
+            x: stat.year_id.substr(0, 4),
+            y: parseInt(stat.pass_yds)
+        }
+    });
+}
+
+class Stats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
+    _forgetValue = () => {
+        this.setState({
+            value: null
+        });
+    };
+
+    _rememberValue = (e) => {
+        console.log('e: ', e);
+        this.setState({ value: e.value });
+    };
+
+    render() {
+        const data = transformData(this.props.stats);
+        const years = []
+        this.props.stats.forEach((stat) => {
+            years.push(stat.year_id.substr(0, 4));
+        });
+        const passYds = []
+        this.props.stats.forEach((stat) => {
+            passYds.push(parseInt(stat.pass_yds));
+        });
+
+        const passTds = [];
+        this.props.stats.forEach((stat) => {
+            passTds.push(parseInt(stat.pass_td));
+        });
+
+        const passInts = [];
+        this.props.stats.forEach((stat) => {
+            passInts.push(parseInt(stat.pass_int));
+        });
+
+        return (
+            <div>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <div style={{ flex: '0 1 50%' }}>
+
+                        <Chart>
+                            <ChartTitle text="Passing Yards" />
+                            <ChartTooltip format="{0}" />
+                            <ChartCategoryAxis>
+                                <ChartCategoryAxisItem title={{ text: 'Years' }} categories={years} />
+                            </ChartCategoryAxis>
+                            <ChartSeries>
+                                <ChartSeriesItem type="line" data={passYds} color={'#203731'} />
+                            </ChartSeries>
+                        </Chart>
+                    </div>
+                    <div style={{ flex: '0 1 50%' }}>
+
+                        <Chart>
+                            <ChartTitle text="Passing Touchdowns" />
+                            <ChartCategoryAxis>
+                                <ChartCategoryAxisItem title={{ text: 'Years' }} categories={years} />
+                            </ChartCategoryAxis>
+                            <ChartSeries>
+                                <ChartSeriesItem type="line" data={passTds} color={'#203731'} />
+                            </ChartSeries>
+                        </Chart>
+                    </div>
+                    <div style={{ flex: '0 1 50%' }}>
+                        <Chart>
+                            <ChartSeries>
+                                <ChartSeriesItem type="bar" stack={true} data={passTds} color={'green'} />
+                                <ChartSeriesItem type="bar" data={passInts} color={'red'} />
+                            </ChartSeries>
+                        </Chart>
+
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default Stats;
